94c066a3ffd8babea411f2999599aab29e264da0
Wendres Lucas Araujo <91164904+wendreslucas@users.noreply.github.com>
2022-06-02 14:01:36 -0300

    ajustes de login, e registro

--- Frontend/src/context/CadasterContext.js
+++ Frontend/src/context/CadasterContext.js
@@ -34,29 +34,28 @@ export const CadasterContextProvider = ({ children }) => {
       })
   }
 
-  const createUser = async data => {
-    const user = {
-      name: data.name,
-      email: data.email,
-      password: data.password
-    }
+  const createUser = (req, res) => {
+    api
+      .post('/user/register', {
+        name: req.name,
+        email: req.email,
+        password: req.password
+      })
+      .then(res => {
+        if (res.status === 201) {
+          toast.success('Usuário criado com sucesso!')
+          navigate('/')
+        }
+      })
+      .catch(err => {
+        if (err.response.status === 400) {
+          toast.error('Ops Email já existe!')
+        }
 
-    const res = await api.post('/user/register', user)
-
-    if (res.status === 200) {
-      toast.error('Ops! Usuário já existe')
-    }
-
-    if (res.status === 201) {
-      toast.success('Yeah! Usuário cadastrado com sucesso')
-      navigate('/')
-    }
-    if (res.status === 400) {
-      toast.error('Ops! Confira os campos digitados!')
-    }
-    if (res.status === 401) {
-      toast.error('Ops! Confira as informações')
-    }
+        if (err.response.status === 404) {
+          toast.error('Ops Username já existe!')
+        }
+      })
   }
 
   return (
